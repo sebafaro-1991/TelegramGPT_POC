d243856cdb0f0e6e074260122761303df7f2faec
Sebastian <78424824+sebafaro-1991@users.noreply.github.com>
2023-04-04 12:52:01 +0000

    Small comments added

--- main.py
+++ main.py
@@ -3,29 +3,40 @@ import os
 import telebot
 import openai
 
+#Get the toke that we saved in the secrets
 BOT_TOKEN = os.environ.get('TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+
 def OpenAi(prompt):
   openai.api_key = os.getenv("OPENAI_API_KEY")
   response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo-0301",
-   messages=[
-        {"role": "system", "content": "You are Change Management expert working in a telecomunication company."},
-        {"role": "user", "content": prompt}
-    ],
-
+    model="gpt-3.5-turbo-0301",
+    messages=[{
+      "role":
+      "system",
+      "content":
+      "You are Change Management expert working in a telecomunication company."
+    }, {
+      "role": "user",
+      "content": prompt
+    }],
   )
   return response.choices[0].message.content
 
+
+#Messages for the start commands
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "You are ready now to start, please say hi to the bot")
+  bot.reply_to(message, "You are ready now to start, please say hi to the bot")
 
 
+#Reply from openAI api
 @bot.message_handler(func=lambda msg: True)
-def echo_all(message):  
-    bot.reply_to(message, OpenAi(message.text))
-    print(message.text)
+def echo_all(message):
+  bot.reply_to(message, OpenAi(message.text))
+  print(message.text)
+
+
 bot.infinity_polling()
